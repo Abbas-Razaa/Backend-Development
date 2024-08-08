562cb6c660e430e29275f6b1d8e0650273a688fc
Abbas Raza <abbasrazaa02@gmail.com>
2024-08-08 23:29:18 +0500

    npm Features

---
+++ NPM Features/script.js
@@ -0,0 +1,16 @@
+// npm install package-name
+// npm uninstall package-name
+// npm install package-name@version
+
+// dependency: packages and dependencies of packages
+
+// dev dependency: dependencies that is used before deployment e.g: nodemon
+
+// default
+// npm start
+// npm test
+
+// npm run: that we have created
+// add in "scripts":{
+//      "name": "node script.js"
+// }
