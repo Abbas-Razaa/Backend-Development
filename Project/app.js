b5520e52ebe4a7971cc5ad9302564389541d7b4f
Abbas Raza <abbasrazaa02@gmail.com>
2024-08-17 01:42:06 +0500

    profile page

--- Project/app.js
+++ Project/app.js
@@ -18,9 +18,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/profile', isLoggedIn, (req,res) => {
-    console.log(req.user);
-    res.render('login');
+    let user = userModel.findOne({email: req.user.email}).populate('posts');
+    res.render('profile', {user});
 })
+
+app.post('/post', isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+    let post =  await postModel.create({
+        user: user._id,
+        content
+    });
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect('/profile');
+});
+
 
 app.get('/register', async (req, res) => {
     let {email,password,username,name,age} = req.body;
@@ -63,7 +76,7 @@ app.get('/login', async (req, res) => {
             let token = jwt.sign({email: email, userid: user
             ._id}, "shhhhh");
             res.cookie('token', token);
-            res.status(200).send('you can login');
+            res.status(200).redirect('/profile');
         }
         else res.redirect('/login');
     });
@@ -75,7 +88,7 @@ app.get('/logout', (req, res) => {
 });
 
 function isLoggedIn(req, res, next) {
-    if(req.cookies.token === "") res.send('You must be logged in');
+    if(req.cookies.token === "") res.redirect('/login');
     else{
         let data = jwt.verify(req.cookies.token, "shhhhh");
         req.user = data;
