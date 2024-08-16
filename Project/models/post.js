5619e1f83ae8ebb8e2f0fa0259b43f77c29455d6
Abbas Raza <abbasrazaa02@gmail.com>
2024-08-17 00:49:43 +0500

    project

---
+++ Project/models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/miniproject');
+
+const postSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    content: String,
+    likes: [
+        { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    ]
+});
+
+module.exports = mongoose.model('post', postSchema);
