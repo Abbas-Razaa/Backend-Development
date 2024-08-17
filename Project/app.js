f0ad14e7e9b9465777521746d72f1564cdab3b11
Abbas Raza <abbasrazaa02@gmail.com>
2024-08-18 00:33:03 +0500

    read & update post

--- Project/app.js
+++ Project/app.js
@@ -22,6 +22,30 @@ app.get('/profile', isLoggedIn, (req,res) => {
     res.render('profile', {user});
 })
 
+app.get('/like/:id', isLoggedIn, async (req,res) => {
+    let post = await userModel.findOne({email: req.user.email}).populate('users');
+
+    if(post.likes.indexOf(req.user.userid) === -1) {
+        post.likes.push(req.user.userid);
+    }
+    else {
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+    await post.save();
+    res.redirect('/profile');
+})
+
+app.get('/edit/:id', isLoggedIn, async (req,res) => {
+    let post = await postModel.findOne({_id: req.params.id}).populate('users');
+    res.render('edit', {post});
+})
+
+app.get('/update/:id', isLoggedIn, async (req,res) => {
+    let post = await postModel.findOneAndUpdate({_id: req.params.id}, { content: req.body.content })
+    res.redirect("/profile");
+})
+
+
 app.post('/post', isLoggedIn, async (req, res) => {
     let user = await userModel.findOne({email: req.user.email});
     let {content} = req.body;
